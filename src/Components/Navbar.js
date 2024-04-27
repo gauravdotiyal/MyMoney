cd8f0aa663c827fc0abb89c4ca9b7a9a35f3c8e9
Gaurav Dotiyal <gauravdotiyal33@gmail.com>
2024-04-27 16:37:59 +0530

    login form added

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar() {
   return (
      <nav className={styles.navbar}>
         <ul>
-            <li className={styles.title}>myMoney</li>
+            <li className={styles.title}><Link to='/'>myMoney</Link></li>
 
             <li> <Link to='/login'>Login</Link></li>
             <li> <Link to='/signup'>Signup</Link></li>
