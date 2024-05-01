912c3533bcb166a85da2449ade75eee485e8d58c
Gaurav Dotiyal <gauravdotiyal33@gmail.com>
2024-05-01 10:59:37 +0530

    dispatch a login action

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -3,15 +3,19 @@ export const AuthContext =createContext()
 
 export const authReducer =(state,action)=>{
     switch (action.type){
+        case'LOGIN':
+           return {...state,user:action.payload}
         default: 
           return state
     }
 }
 
 export const AuthContextProvider=({children})=>{
-    const [tstate ,dispatch ]= useReducer(authReducer,{
+    const [state ,dispatch ]= useReducer(authReducer,{
         user:null
     })
+
+    console.log('AuthContext state:',state)
     
     return (
         <AuthContext.Provider value={{...state ,dispatch}}>
